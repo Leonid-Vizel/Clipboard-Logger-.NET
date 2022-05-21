30edd2631e2b867e4aaa4c32437b74883976c876
Vizel Leonid <spartanlaska@gmail.com>
2022-05-21 16:49:48 +0300

    Добавьте файлы проекта.

---
+++ ClipboardLogger/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ClipboardLogger
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clipList = new System.Windows.Forms.ListBox();
+            this.clipLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clipList
+            // 
+            this.clipList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.clipList.FormattingEnabled = true;
+            this.clipList.ItemHeight = 21;
+            this.clipList.Location = new System.Drawing.Point(13, 41);
+            this.clipList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.clipList.Name = "clipList";
+            this.clipList.Size = new System.Drawing.Size(452, 403);
+            this.clipList.TabIndex = 0;
+            // 
+            // clipLabel
+            // 
+            this.clipLabel.AutoSize = true;
+            this.clipLabel.Location = new System.Drawing.Point(13, 15);
+            this.clipLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.clipLabel.Name = "clipLabel";
+            this.clipLabel.Size = new System.Drawing.Size(459, 21);
+            this.clipLabel.TabIndex = 1;
+            this.clipLabel.Text = "Logs the clipboard copy action even when this form is minimised";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 469);
+            this.Controls.Add(this.clipLabel);
+            this.Controls.Add(this.clipList);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(494, 508);
+            this.Name = "MainForm";
+            this.Text = "Clipboard Logger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox clipList;
+        private System.Windows.Forms.Label clipLabel;
+    }
+}
---
+++ ClipboardLogger/MainForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ClipboardLogger
+{
+    public partial class MainForm : Form
+    {
+        private const int WM_DRAWCLIPBOARD = 0X308;
+        private const int WM_CHANGECBCHAIN = 0X030D;
+
+        public MainForm()
+        {
+            nextClipboardViewer = (IntPtr)SetClipboardViewer((int)Handle);
+            SetProcessDpiAwarenessContext(-1);
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDpiAwarenessContext(int value);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern short GetKeyState(int keyCode);
+
+        [DllImport("user32.dll")]
+        protected static extern int SetClipboardViewer(int hWndNewViewer);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        IntPtr nextClipboardViewer;
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    DisplayClipboardData();
+                    SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    break;
+                case WM_CHANGECBCHAIN:
+                    if (m.WParam == nextClipboardViewer)
+                    {
+                        nextClipboardViewer = m.LParam;
+                    }
+                    else
+                    {
+                        SendMessage(nextClipboardViewer, m.Msg, m.WParam, m.LParam);
+                    }
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        private void DisplayClipboardData()
+        {
+            string strClip = string.Empty;
+            try
+            {
+                IDataObject dataObject = new DataObject();
+                dataObject = Clipboard.GetDataObject();
+                if (dataObject.GetDataPresent(DataFormats.UnicodeText) && (dataObject.GetData(DataFormats.UnicodeText).ToString() != string.Empty))
+                {
+                    strClip = dataObject.GetData(DataFormats.UnicodeText).ToString();
+                }
+                else if (dataObject.GetDataPresent(DataFormats.Text) && ((string)dataObject.GetData(DataFormats.StringFormat) != string.Empty))
+                {
+                    strClip = (string)dataObject.GetData(DataFormats.StringFormat);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            clipList?.Items.Add(strClip);
+        }
+    }
+}
